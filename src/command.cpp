6720f63b279dcbbcf320911d541fa693db56fce6
LemoeChun <lemoechun@outlook.com>
2025-08-20 19:26:31 +0800

    初始化仓库

---
+++ src/command.cpp
@@ -0,0 +1,93 @@
+#include <cstddef>
+#include <filesystem>
+#include <fstream>
+#include <functional>
+#include <string>
+#include <unordered_map>
+#include <cstdlib>
+
+#include <yaml-cpp/yaml.h>
+
+#include "command.h"
+
+void about(json &resp_msg,json &msg,std::string &arg){
+    resp_msg["params"]["message"][0]["type"] = "text";
+    resp_msg["params"]["message"][0]["data"]["text"] = "你好喵，这里是帕酱～";
+}
+
+void jm(json &resp_msg,json &msg,std::string &arg){
+    std::string download_dir = "/home/lemoechun/bot/pdf/" + arg; 
+    YAML::Node options;
+    options["plugins"]["after_album"][0]["plugin"] = "img2pdf";
+    options["plugins"]["after_album"][0]["kwargs"]["pdf_dir"] = download_dir;
+    options["plugins"]["after_album"][0]["kwargs"]["filename_rule"] = "Aname";
+    // options["plugins"]["after_album"][0]["kwargs"]["encrypt"]["password"] = "nyanyanya";
+    std::ofstream jm_option_file("jm_option.yml");
+    jm_option_file.clear();
+    jm_option_file << options;
+    jm_option_file.close();
+    std::filesystem::create_directories(download_dir);
+    std::string jm_command = "jmcomic --option jm_option.yml " + arg;
+    if ( (std::system(jm_command.data())) !=0 ){
+        resp_msg["params"]["message"][0]["type"] = "text";
+        resp_msg["params"]["message"][0]["data"]["text"] = "hentai！一天到晚看本子真是没救了喵";
+        return;
+    };
+    std::string filename = std::filesystem::path((*std::filesystem::directory_iterator("/home/lemoechun/bot/pdf/" + arg))).filename().string();
+    if (msg["message_type"]=="group"){
+        resp_msg["action"] = "upload_group_file";
+    } else {
+        resp_msg["action"] = "upload_private_file";
+    }
+    resp_msg["params"].erase("message");
+    resp_msg["params"].erase("message_type");
+    resp_msg["params"]["name"] = filename;
+    resp_msg["params"]["file"] = download_dir + "/" + filename;
+    
+    
+}
+/**
+void get_file(json &resp_msg,json &msg,std::string &arg){
+    if (msg["message_type"]=="group"){
+        resp_msg["action"] = "upload_group_file";
+    } else {
+        resp_msg["action"] = "upload_private_file";
+    }
+    resp_msg["params"].erase("message");
+    resp_msg["params"].erase("message_type");
+    
+    std::string filename = std::filesystem::path(arg).filename().string();
+    resp_msg["params"]["name"] = filename;
+    resp_msg["params"]["file"] = arg;
+}
+**/
+json ProcessCommand(json &msg){
+    auto raw_message = std::string(msg["raw_message"]);
+    std::string command,arg;
+    std::unordered_map<std::string, std::function<void(json&,json&,std::string&)>> commands;
+    commands["about"] = about;
+    commands["jm"] = jm;
+//    commands["get_file"] = get_file;
+    if (raw_message[0] == '>'){
+        json resp_msg;
+        resp_msg["action"] = "send_msg";
+        resp_msg["params"]["message_type"] = msg["message_type"];
+        if (msg["message_type"]=="group"){
+            resp_msg["params"]["group_id"] = msg["group_id"];
+        }
+        resp_msg["params"]["user_id"] = msg["user_id"];
+        size_t first_space = raw_message.find(" ");
+        if (first_space != std::string::npos){
+            command = raw_message.substr(1,first_space-1);
+            arg = raw_message.substr(first_space+1);
+        } else {
+            command = raw_message.substr(1);
+            arg = "";
+        }
+        commands[command](resp_msg,msg,arg);
+        return resp_msg;
+    };
+    
+    return nlohmann::json();
+}
+
---
+++ src/main.cpp
@@ -0,0 +1,81 @@
+/* We simply call the root header file "App.h", giving you uWS::App and uWS::SSLApp */
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <sstream>
+
+#include <uWebSockets/App.h>
+#include <nlohmann/json.hpp>
+#include <toml++/toml.hpp>
+
+#include "command.h"
+/* This is a simple WebSocket echo server example.
+ * You may compile it with "WITH_OPENSSL=1 make" or with "make" */
+
+using json = nlohmann::json;
+
+
+int main() {
+    /* ws->getUserData returns one of these */
+    struct PerSocketData {
+        /* Fill with user data */
+    };
+
+    /* Keep in mind that uWS::SSLApp({options}) is the same as uWS::App() when compiled without SSL support.
+     * You may swap to using uWS:App() if you don't need SSL */
+    uWS::App(
+        /* There are example certificates in uWebSockets.js repo */
+    ).ws<PerSocketData>("/*", {
+        /* Settings */
+        .compression = uWS::CompressOptions(uWS::DEDICATED_COMPRESSOR | uWS::DEDICATED_DECOMPRESSOR),
+        .maxPayloadLength = 100 * 1024 * 1024,
+        .idleTimeout = 16,
+        .maxBackpressure = 100 * 1024 * 1024,
+        .closeOnBackpressureLimit = false,
+        .resetIdleTimeoutOnSend = false,
+        .sendPingsAutomatically = true,
+        /* Handlers */
+        .upgrade = nullptr,
+        .open = [](auto *ws) {
+            /* Open event here, you may access ws->getUserData() which points to a PerSocketData struct */
+
+        },
+        .message = [](auto *ws, std::string_view message, uWS::OpCode opCode) {
+            /* This is the opposite of what you probably want; compress if message is LARGER than 16 kb
+             * the reason we do the opposite here; compress if SMALLER than 16 kb is to allow for 
+             * benchmarking of large message sending without compression */
+
+             /* Never mind, it changed back to never compressing for now */
+            //std::cout << message << "\n";
+            //ws->send(message, opCode, false);
+            auto msg = json::parse(message);
+            std::cout << msg.dump(4) << std::endl;
+            if (msg["post_type"] == "message"){
+                json resp_msg = ProcessCommand(msg);
+                if (!resp_msg.empty()){
+                    std::cout << resp_msg.dump(4) << std::endl;
+                    ws->send(resp_msg.dump());
+                }
+            }
+        },
+        .dropped = [](auto */*ws*/, std::string_view /*message*/, uWS::OpCode /*opCode*/) {
+            /* A message was dropped due to set maxBackpressure and closeOnBackpressureLimit limit */
+        },
+        .drain = [](auto */*ws*/) {
+            /* Check ws->getBufferedAmount() here */
+        },
+        .ping = [](auto */*ws*/, std::string_view) {
+            /* Not implemented yet */
+        },
+        .pong = [](auto */*ws*/, std::string_view) {
+            /* Not implemented yet */
+        },
+        .close = [](auto */*ws*/, int /*code*/, std::string_view /*message*/) {
+            /* You may access ws->getUserData() here */
+        }
+    }).listen(3100, [](auto *listen_socket) {
+        if (listen_socket) {
+            std::cout << "Listening on port " << 3100 << std::endl;
+        }
+    }).run();
+}
