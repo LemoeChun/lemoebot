3eaa0f038c428a097548c56a2a07a6c192561859
LemoeChun <lemoechun@outlook.com>
2025-11-13 09:56:21 +0800

    feat: 发送消息过长转化为转发聊天记录

--- src/milky_api.cpp
+++ src/milky_api.cpp
@@ -9,24 +9,34 @@
 #include "milky_api.hpp"
 #include "config.hpp"
 
+using nlohmann::json;
+
 auto json_header = cpr::Header{{"Content-Type", "application/json"}};
 const std::string api_base_url = config["Bot"]["Milky_API_URL"].value_or<std::string>("http://127.0.0.1:3100/api");
 
-cpr::Response CallApi(std::string &api,nlohmann::json &msg){
+cpr::Response CallApi(std::string &api,json &msg){
     std::string api_url(api_base_url + api);
     HX::log::hxLog.info("将向 ",api_url, " 发送：\n",msg.dump(4));
     return cpr::Post(cpr::Url{api_url},json_header,cpr::Body{msg.dump()});
 }
 
-cpr::Response SendMsg(nlohmann::json &msg){
+cpr::Response SendMsg(json &msg){
     std::string api_url = api_base_url;
     if (msg.contains("group_id")) api_url.append("/send_group_message");
     if (msg.contains("user_id")) api_url.append("/send_private_message");
+    if (msg["message"][0]["type"] == "text" 
+            && msg["message"][0]["data"]["text"].get<std::string>().length() > 500 ) {
+        json forward_msg = msg;
+        forward_msg["message"][0]["type"] = "forward";
+        forward_msg["message"][0]["data"]["messages"]["segments"] = msg["message"];
+        forward_msg["message"][0]["user_id"] = config["Bot"]["QQ_ID"].value_or<uint>(1);
+        forward_msg["message"][0]["sender_name"] = config["Bot"]["QQ_Name"].value_or<std::string>("喵呜");
+    }
     HX::log::hxLog.info("将向 ",api_url, " 发送：\n",msg.dump(4));
     return cpr::Post(cpr::Url{api_url},json_header,cpr::Body{msg.dump()});
 }
 
-cpr::Response UpLoadFile(nlohmann::json &msg){
+cpr::Response UpLoadFile(json &msg){
     std::string api_url = api_base_url;
     if (msg.contains("group_id")) api_url.append("/upload_group_file");
     if (msg.contains("user_id")) api_url.append("/upload_private_file");
